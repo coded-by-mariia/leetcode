8f46b4dc51e71b2462d342dde8960c6e9f1f8c30
mariia-bur <mariia.burdeinaia@gmail.com>
2024-06-21 19:29:04 +0100

    init commit

---
+++ 13_roman_to_int/main.cpp
@@ -0,0 +1,54 @@
+/***********************************************************
+ * Task 13 "Roman To Integer"
+ * 
+ * Roman numerals are represented by seven 
+ * different symbols: I (1), V (5), X (10), L (50), C (100), 
+ * D (500) and M (1000).
+ 
+ * There are six instances where subtraction is used:
+ * I can be placed before V (5) and X (10) to make 4 and 9. 
+ * X can be placed before L (50) and C (100) to make 40 and 90. 
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+***********************************************************/
+
+#include <map>
+#include <iostream>
+
+void vector_solution(const std::vector<std::string> &);
+void solution(const std::string &);
+
+void vector_solution(const std::vector<std::string> &roman_nums) {
+    for (const auto &num : roman_nums) {
+        solution(num);
+    }
+}
+
+void solution(const std::string &roman) {
+    std::unordered_map<char,int> dict = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+    int total {0};
+
+    for (int i {0}; i < roman.size(); i++) {
+        if (i < (roman.size() - 1) && dict[roman.at(i)] < dict[roman.at(i+1)]) {
+            total -= dict[roman.at(i)];
+        } else {
+            total += dict[roman.at(i)];
+        }
+    }
+
+    std::cout << roman << " : " << total << std::endl;
+}
+
+int main() {
+    std::vector<std::string> roman_nums {"III", "IV", "IX", "X", "XXVII", "MMDLV"};
+    vector_solution(roman_nums);
+    return 0;
+}
---
+++ 66_plus_one/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> plus_one(std::vector<int> &number) {
+    auto it = number.end() - 1;
+
+    while (true) {
+        if (*it == 9) {
+            *it = 0;
+            if (it == number.begin()) {
+                number.insert(number.begin(), 1);
+                break;
+            }
+        it--;
+        } else {
+            *it += 1;
+            break;
+        }
+    }
+    return number;
+}
+
+void display_vector(const std::vector<int> &vec) {
+    std::cout << "\n[ ";
+    for (const auto &dig : vec) {
+        std::cout << dig << " ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+
+int main() {
+    std::vector<std::vector<int>> vec_nums;
+    vec_nums.push_back({1,2,3,4});
+    vec_nums.push_back({9});
+    vec_nums.push_back({9,9,9,9});
+    vec_nums.push_back({1,8,9});
+    vec_nums.push_back({1,9,3,4});
+    vec_nums.push_back({9,2,3,4});
+ 
+    for (auto &number : vec_nums) {
+        std::cout << "\nVector before incremetion: ";
+        display_vector(number);
+        plus_one(number);
+        std::cout << "Vector after incremention: ";
+        display_vector(number);
+    }
+   
+    return 0;
+}
